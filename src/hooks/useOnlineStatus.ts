e5333fc1fe3bd55721e43ad98dae0eda7627563c
armn11155-ctrl <armn.11155@gmail.com>
2026-05-22 11:48:08 -0500

    style: prettier format useOnlineStatus.ts

--- src/hooks/useOnlineStatus.ts
+++ src/hooks/useOnlineStatus.ts
@@ -11,14 +11,14 @@ export function useOnlineStatus(): boolean {
   );
 
   useEffect(() => {
-    const goOnline  = () => setOnline(true);
+    const goOnline = () => setOnline(true);
     const goOffline = () => setOnline(false);
 
-    window.addEventListener("online",  goOnline);
+    window.addEventListener("online", goOnline);
     window.addEventListener("offline", goOffline);
 
     return () => {
-      window.removeEventListener("online",  goOnline);
+      window.removeEventListener("online", goOnline);
       window.removeEventListener("offline", goOffline);
     };
   }, []);
