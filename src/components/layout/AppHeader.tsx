b7a8a0bb46af8e0c0f791133c2ba2ed8a0695427
armn11155-ctrl <armn.11155@gmail.com>
2026-05-26 16:19:47 -0500

    style: prettier AppHeader.tsx

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -77,9 +77,9 @@ export function AppHeader({
         }}
       >
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
-          <rect x="3"  y="3"  width="8" height="8" rx="2" fill="white" />
-          <rect x="13" y="3"  width="8" height="8" rx="2" fill="white" />
-          <rect x="3"  y="13" width="8" height="8" rx="2" fill="white" />
+          <rect x="3" y="3" width="8" height="8" rx="2" fill="white" />
+          <rect x="13" y="3" width="8" height="8" rx="2" fill="white" />
+          <rect x="3" y="13" width="8" height="8" rx="2" fill="white" />
           <rect x="13" y="13" width="8" height="8" rx="2" fill="white" />
         </svg>
       </button>
